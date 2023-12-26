a0d705c466d7cc64dee778330af4d201d7f28b10
songxinglin <songxingling1@outlook.com>
2023-12-26 19:34:28 +0800

    实现完了queue

--- No.3/2.queue_list.cpp
+++ No.3/2.queue_list.cpp
@@ -6,8 +6,25 @@ private:
     List *l;
 public:
     Queue():l(new List) {};
+    ~Queue() {
+        delete l;
+    }
     int getSize() {return l->getSize();}
-
+    bool empty() {
+        return l->getSize() == 0;
+    } 
+    int push(int val) {
+        if(l->push_back(val) == -1) return -1;
+        return 0;
+    }
+    int pop() {
+        if(l->pop_front() == -1) return -1;
+        return 0;
+    }
+    int front() {
+        if(empty(l)) return -1;
+        return l[1];
+    }
 };
 
 int main() {
