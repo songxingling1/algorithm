0d2aaab94d284f6458f98eea175e776e224369f9
songxinglin <songxingling1@outlook.com>
2023-12-26 18:57:58 +0800

    完成queue的结构定义

---
+++ No.3/2.queue_list.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Queue {
+private:
+    List *l;
+public:
+    Queue():l(new List) {};
+    int getSize() {return l->getSize();}
+
+};
+
+int main() {
+    return 0;
+}
