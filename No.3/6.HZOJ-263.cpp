cab33f838602a9b4edd26a6527802444785f7baf
songxinglin <songxingling1@outlook.com>
2024-01-04 21:00:50 +0800

    更新题目

--- No.3/6.HZOJ-263.cpp
+++ No.3/6.HZOJ-263.cpp
@@ -1,50 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n;
-stack<int> sta;
-vector<int> ans;
-stack<string> a;
-void makeAnswer() {
-    string as;
-    for(int i = 0;i < ans.size();i++) {
-        as += to_string(ans[i]);
+bool isValid(int a[],int n) {
+    stack<int> s;
+    int x = 1;
+    for(int i = 0;i < n;i++) {
+        if(s.empty() || s.top() < a[i]) {
+            while(x <= a[i]) s.push(x),x++;
+        }
+        if(s.empty() || s.top() != a[i]) return false;
+        s.pop();
     }
-    a.push(as);
-}
-void solve(int step,int top) {
-    if(step > 2 * n) {
-        makeAnswer();
-        return;
-    }
-    if(top > n) {
-        int temp = sta.top();
-        ans.push_back(temp);
-        sta.pop();
-        solve(step + 1,top);
-        sta.push(temp);
-        ans.pop_back();
-        return;
-    }
-    sta.push(top);
-    solve(step + 1,top + 1);
-    sta.pop();
-    if(!sta.empty()) {
-        int temp = sta.top();
-        ans.push_back(temp);
-        sta.pop();
-        solve(step + 1,top);
-        sta.push(temp);
-        ans.pop_back();
-    }
+    return true;
 }
 int main() {
+    int n,a[25],cnt = 20;
     scanf("%d",&n);
-    solve(1,1);
-    int i = 0;
-    while((!a.empty()) && i < 20) {
-        printf("%s\n",(a.top()).c_str());
-        a.pop();
-        i++;
-    }
+    for(int i = 0;i < n;i++) a[i] = i + 1;
+    do
+    {
+        if(isValid(a,n)) {
+            for(int i = 0;i < n;i++) {
+                printf("%d",a[i]);
+            }
+            puts("");
+            cnt--;
+        }
+    } while (next_permutation(a,a + n) && cnt);
     return 0;
 }
