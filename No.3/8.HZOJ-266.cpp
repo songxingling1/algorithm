b2ce5fb045a5b2860f342b8f952581eecf0a42a8
songxinglin <songxingling1@outlook.com>
2024-01-06 21:13:21 +0800

    更新一道题

--- No.3/8.HZOJ-266.cpp
+++ No.3/8.HZOJ-266.cpp
@@ -1,43 +1,17 @@
 #include <bits/stdc++.h>
-#define MAX_S 1000
-char s[MAX_S + 5];
-bool isDelete[MAX_S + 5] = {false};
+#define MAX_N 1000
 using namespace std;
-struct node {
-    bool is;
-    int num;
-    char op;
-};
-int judge(char c) {
-    if(c >= '0' && c <= '9') return 0;
-    if(c == '(' || c == ')') return -1;
-    if(c == '^') return 3;
-    if(c == '*' || c == '/') return 2;
-    return 1;
-}
-int judge_c(int pos) {
-    if(isDelete[pos]) return 1;
+char s[MAX_N + 5];//字符串
+int m[MAX_N + 5] = {0}; //权
+int judge(int pos) { //判断运算符层级
+    if(s[pos] == '-' && (s[pos - 1] == '(' || pos == 1 || (!isdigit(s[pos - 1]))) && s[pos - 1] != ')') return 4;
     char c = s[pos];
-    if(c >= '0' && c <= '9') return 0;
-    if(c == ')') return -1;
-    if(c == '(') return -2;
-    if(c == '^') return 3;
+    if(c == '+' || c == '-') return 1;
     if(c == '*' || c == '/') return 2;
-    return 1;
+    if(c == '^') return 3;
+    return 4;
 }
-pair<int,int> getNum(int pos) {
-    int i = pos;
-    int ans = 0,t;
-    if(s[i] == '-') i++;
-    while(judge(s[i]) == 0) {
-        t = s[i] - '0';
-        ans = ans * 10 + t;
-        i++;
-    }
-    if(s[pos] == '-') ans *= -1;
-    return make_pair(ans,i);
-}
-int qpow(int a,int b) {
+int qpow(int a,int b) { //快速幂
     int ans = 1;
     while(b) {
         if(b & 1) ans *= a;
@@ -46,132 +20,63 @@ int qpow(int a,int b) {
     }
     return ans;
 }
-int clac(vector<node> num) {
-    vector<int> temp;
-    int t = num[0].num;
-    for(int i = 0;i < num.size();i++) {
-        if(!num[i].is) {
-            temp.push_back(num[i].num);
-            continue;
-        }
-        switch(num[i].op) {
-            case '^':
-                t = qpow(temp[temp.size() - 2],temp[temp.size() - 1]);break;
-            case '*':
-                t = temp[temp.size() - 2] * temp[temp.size() - 1];break;
-            case '/':
-                t = temp[temp.size() - 2] / temp[temp.size() - 1];break;
-            case '+':
-                t = temp[temp.size() - 2] + temp[temp.size() - 1];break;
-            case '-':
-                t = temp[temp.size() - 2] - temp[temp.size() - 1];break;
-        }
-        temp.pop_back();
-        temp.pop_back();
-        temp.push_back(t);
-    }
-    return t;
-}
-int solve() {
+void str() { //计算权值
+    int isK = 0;
     int size = strlen(s);
-    int i = 1;
-    node fd;
-    vector<node> num;
-    pair<int,int> getN;
-    stack<char> op;
-    op.push('\0');
-    while(i < size) {
-        if(isDelete[i]) {i++;continue;}
-        if(s[i] >= '0' && s[i] <= '9') {
-            getN = getNum(i);
-            fd.is = false;
-            fd.num = getN.first;
-            num.push_back(fd);
-            i = getN.second;
-            continue;
-        }else if (s[i] == '-' && (isDelete[i - 1] || judge(s[i - 1]) != 0) && judge_c(i - 1) != -1) {
-            getN = getNum(i);
-            fd.is = false;
-            fd.num = getN.first;
-            num.push_back(fd);
-            i = getN.second;
-            continue;
-        } else if(s[i] == '(') {
-            op.push(s[i]);
-            i++;
-            continue;
-        } else if(s[i] == ')') {
-            while(op.top() != '(' && op.size() > 1) {
-                fd.is = true;
-                fd.op = op.top();
-                num.push_back(fd);
-                op.pop();
-            }
-            op.pop();
-            i++;
-            continue;
-        }else if(judge(s[i]) == 3) {
-            while(op.size() > 1 && judge(op.top()) == 3) {
-                fd.is = true;
-                fd.op = op.top();
-                op.pop();
-                num.push_back(fd);
-            }
-            op.push(s[i]);
-            i++;
-            continue;
-        }else if(judge(s[i]) == 2) {
-            while(op.size() > 1 && judge(op.top()) >= 2) {
-                fd.is = true;
-                fd.op = op.top();
-                op.pop();
-                num.push_back(fd);
-            }
-            op.push(s[i]);
-            i++;
-            continue;
-        }else if(judge(s[i]) == 1) {
-            while(op.size() > 1 && judge(op.top()) >= 1) {
-                fd.is = true;
-                fd.op = op.top();
-                op.pop();
-                num.push_back(fd);
-            }
-            op.push(s[i]);
-            i++;
-            continue;
-        }
-    }
-    while(op.size() > 1) {
-        fd.is = true;
-        fd.op = op.top();
-        op.pop();
-        num.push_back(fd);
-    }
-    return clac(num);
-}
-void removeExcessParentheses() {
-    vector<int> sta;
-    int size = strlen(s);
+    /**********************   第一部分：遍历字符串   ***************************/
     for(int i = 1;i < size;i++) {
-        if(s[i] != '(' && s[i] != ')') continue;
-        if(s[i] == '(') {
-            sta.push_back(i);
-        } else if(s[i] == ')') {
-            if(sta.empty()) isDelete[i] = true;
-            else sta.pop_back();
-        }
-    }
-    for(int i:sta) {
-        isDelete[i] = true;
+        if(s[i] == '(') isK += 100; //bug1:权值没有考虑到有多重括号的式子 (isk = true)
+        m[i] = judge(i) + isK;
+        if(s[i] == ')' && isK != 0) isK -= 100;//bug1:权值没有考虑到有多重括号的式子 (isk = false)
     }
 }
-int main()
-{
+int isNumber(int begin,int end) { //判断是否为数字：不是返回-1             是返回数字的值
+    int ans = 0;
+    int dr = 0;
+    if(!(isdigit(s[begin]) || s[begin] == '-') && s[begin] != '(' && s[begin] != ')') return -1;
+    if(s[begin] == '-') dr = -1;
+    if(isdigit(s[begin])) ans = (int)s[begin] - '0'; //bug4:没有考虑到第一位是数字的情况 (delete)
+    for(int i = begin + 1;i <= end;i++) {
+        if(s[i] == '(' || s[i] == ')') continue;
+        if(!isdigit(s[i]) && !(dr == 0 && s[i] == '-')) return -1; //bug3:没有考虑到'-'不在第一位的情况 (if(!isdigit(s[i])) return -1;)
+        if(dr == 0 && s[i] == '-') dr = -1;
+        else ans = ans * 10 + int(s[i] - '0');
+    }
+    if(dr == -1) ans *= -1;
+    return ans;
+}
+int solve(int __begin,int __end) { //主要解决方案
+    /*******************   边界   ******************/
+    int nums = isNumber(__begin,__end);
+    if(nums != -1) return nums;
+    /**********************   第一部分：遍历字符串   ***************************/
+    //在79行执行过了！！！
+    /***************************   第二部分：取最小值    ***************************************/
+    int pos = 0,num = 0x3f3f3f3f;
+    for(int i = __begin;i <= __end;i++) {
+        if(s[i] == '(' || s[i] == ')') continue;
+        if(num >= m[i]) { //bug2:没有从左往右计算 (num > m[i])
+            num = m[i];
+            pos = i;
+        }
+    }
+    /*********************    第三部分：分割    ***********************/
+    int a = solve(__begin,pos - 1);
+    int b = solve(pos + 1,__end);
+    /*********************   第四部分：计算   ***********************/
+    switch(s[pos]) {
+        case '^':return qpow(a,b);
+        case '+':return a + b;
+        case '-':return a - b;
+        case '*':return a * b;
+        case '/':return a / b;
+        default:return -1;
+    }
+}
+int main() {
     s[0] = '0';
-    isDelete[0] = true;
     scanf("%s",s + 1);
-    removeExcessParentheses();
-    cout << solve() << endl;
+    str();
+    printf("%d\n",solve(1,strlen(s) - 1));
     return 0;
 }
