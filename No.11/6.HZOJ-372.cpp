9d4925e003be94afeb53ced41222e72b6e336869
songxingling1 <songxingling1@outlook.com>
2024-04-07 21:12:38 +0800

    做题

--- No.11/6.HZOJ-372.cpp
+++ No.11/6.HZOJ-372.cpp
@@ -1,25 +1,23 @@
 #include <bits/stdc++.h>
+#define MAX_N 500000
 using namespace std;
-int main () {
-    int n;
-    scanf ("%d", &n);
-    vector<int> arrA (n + 1), arrB (n + 1);
-    for (int i = 1; i <= n; i++)
-        scanf ("%d", &arrA[i]);
-    for (int i = 1; i <= n; i++)
-        scanf ("%d", &arrB[i]);
-    stack<int> sta, stb;
-    int p;
-    for (p = 1; p <= n; p++) {
-        while (!sta.empty () && arrA[sta.top ()] >= arrA[p])
-            sta.pop ();
-        while (!stb.empty () && arrB[stb.top ()] >= arrB[p])
-            stb.pop ();
-        sta.push (p);
-        stb.push (p);
-        if (sta.size () != stb.size ())
+int arra[MAX_N + 5],arrb[MAX_N + 5];
+int main() {
+    int n;// NOTE : 输入
+    scanf("%d",&n);
+    for(int i = 1;i <= n;i++) scanf("%d",&arra[i]);
+    for(int i = 1;i <= n;i++) scanf("%d",&arrb[i]);
+    stack<int> sta,stb; // NOTE : 处理
+    int i = 1;
+    for(;i <= n;i++) {
+        while(!sta.empty() && arra[sta.top()] >= arra[i]) sta.pop();
+        while(!stb.empty() && arrb[stb.top()] >= arrb[i]) stb.pop();
+        sta.push(i);
+        stb.push(i);
+        if(sta.size() != stb.size()) {
             break;
+        }
     }
-    printf ("%d\n", p - 1);
+    printf("%d\n",i - 1); //NOTE : 输出
     return 0;
 }
