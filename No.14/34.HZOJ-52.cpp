df87e8dd2f7c08f30213fcc1fd8466729c11ce4d
songxingling1 <songxingling1@outlook.com>
2024-05-22 21:23:17 +0800

    独立做题

--- No.14/34.HZOJ-52.cpp
+++ No.14/34.HZOJ-52.cpp
@@ -8,7 +8,7 @@ int n,M;
 double slope(int i,int j) {
     return 1.0 * (f[i] - f[j]) / (sum[i] - sum[j]);
 }
-void setvv(int i,int j) {
+void sets(int i,int j) {
     dp[i] = dp[j] + S(sum[i] - sum[j]) + M;
     f[i] = dp[i] + S(sum[i]);
 }
@@ -22,8 +22,8 @@ int main() {
     q.push_back(0);
     for(int i = 1;i <= n;i++) {
         while(q.size() >= 2 && slope(q[1],q.front()) < 2 * sum[i]) q.pop_front();
-        setvv(i,q.front());
-        while(q.size() >= 2 && slope(q.back(), q[q.size() - 2]) > slope(i,q.back())) q.pop_back();
+        sets(i,q.front());
+        while(q.size() >= 2 && slope(i,q.back()) < slope(q.back(),q[q.size() - 2])) q.pop_back();
         q.push_back(i);
     }
     printf("%lld\n",dp[n]);
