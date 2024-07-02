fa740dd3629e7baa970fbed3242257b70f80355e
songxingling1 <songxingling1@outlook.com>
2024-07-02 14:11:29 +0800

    测试clang-format

--- xinyoudui/No.2/1178.cpp
+++ xinyoudui/No.2/1178.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int n,k;
-    scanf("%d%d",&n,&k);
-    if(k == 0 || k == n) {
+    int n, k;
+    scanf("%d%d", &n, &k);
+    if (k == 0 || k == n) {
         printf("0 0\n");
         return 0;
     }
     int ans;
-    if(3LL * k <= n) {
+    if (3LL * k <= n) {
         ans = 2 * k;
     } else {
         ans = n - k;
     }
-    printf("1 %d\n",ans);
+    printf("1 %d\n", ans);
     return 0;
 }
